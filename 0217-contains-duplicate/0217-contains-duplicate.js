e39601eee4e47c829b23820fca53e896f39c7443
SangJun <73819071+IM0000@users.noreply.github.com>
2025-06-09 17:23:56 +0900

    Time: 8 ms (98.28%), Space: 72.8 MB (78.15%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const numSet = new Set();
+    for(let i=0; i<nums.length; i++) {
+        let num = nums[i];
+        if(numSet.has(num)) {
+            return true;
+        }
+        numSet.add(num);
+    }
+    return false;
+};
